3d723a255298d16f529af5dfd14638a1e965a1d2
Nm-Tan <47451965+Tantannn@users.noreply.github.com>
2025-11-01 08:37:58 +0700

    update bg

--- scripts/Deadzone.cs
+++ scripts/Deadzone.cs
@@ -7,7 +7,7 @@ public partial class Deadzone : Area2D
 {
 	private void _OnBodyEntered(Node2D body)
 	{
-		GD.Print($"Body entered deadzone: {body.Name}");
+		GD.Print($"Body entered deadzone: {body.Name}");t
 		var timer = GetTree().CreateTimer(1.0f); 
 		timer.Timeout += OnTimeout;
 	}
